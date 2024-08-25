b16b6ec845bdeabd9e2e7b8aae8fbbc8094c9aec
Diogo Santos <135476673+DiogoSantoszxx@users.noreply.github.com>
2024-08-25 18:42:34 -0300

    Add files via upload

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
+        Scanner scanner = new Scanner(System.in);
+    
+        //Primeira parte
         
+        System.out.println("Qual seu nome?");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Digite os digitos de sua agencia?");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Digite o número de sua conta?");
+        int conta = scanner.nextInt();
+
+        System.out.println("Qual seu saldo?");
+        float saldo = scanner.nextFloat();
+
+        //Segunda parte resultado
+
+        System.out.println("Olá " + nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é " + agencia +", conta " + conta + " e seu saldo " +saldo+ " já está disponível para saque.");
+
     }
 }
